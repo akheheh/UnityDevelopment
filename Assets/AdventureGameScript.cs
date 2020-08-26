d90badcd2ed122222d096c5a740a7d1c0e9a2bc1
Alexander Heidarian <alexanderheidarian@Alexanders-MacBook-Pro.local>
2020-08-26 16:27:23 -0700

    Added quitting functionality

--- Assets/AdventureGameScript.cs
+++ Assets/AdventureGameScript.cs
@@ -33,5 +33,9 @@ public class AdventureGameScript : MonoBehaviour
         }
 
         TextComponent.text = state.GetStateStory();
+
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
     }
 }
